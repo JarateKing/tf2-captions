9e7adaf37a8fefa16c96439157c197e29b07655c
JarateKing <scmacintosh@upei.ca>
2019-02-18 12:13:07 -0400

    made compile.bat support drag-and-drop files

--- resources/compile.bat
+++ resources/compile.bat
@@ -1,4 +1,8 @@
 :: This is a kinda messy .bat script to compile your custom captions.
 :: Make sure to update the path if needed.
 
-"C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\bin\captioncompiler.exe" "%cd%\closecaption_custom.txt"
+:: either use closecaption_custom.txt, or drag-and-drop the file into compile.bat
+SET "closecaption_txt=%cd%\closecaption_custom.txt"
+if NOT [%1]==[] SET "closecaption_txt=%~1"
+
+"C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\bin\captioncompiler.exe" "%closecaption_txt%"
