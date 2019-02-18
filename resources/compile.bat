2ce2b3d41249af492cd1ae24895f7d4a2c2e1ed7
JarateKing <scmacintosh@upei.ca>
2019-02-18 11:48:15 -0400

    made compile.bat relative to current directory

--- resources/compile.bat
+++ resources/compile.bat
@@ -1,4 +1,4 @@
 :: This is a kinda messy .bat script to compile your custom captions.
 :: Make sure to update the path if needed.
 
-"C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\bin\captioncompiler.exe" "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf\custom\captions\resource\closecaption_custom.txt"
+"C:\Program Files (x86)\Steam\SteamApps\common\Team Fortress 2\bin\captioncompiler.exe" "%cd%\closecaption_custom.txt"
